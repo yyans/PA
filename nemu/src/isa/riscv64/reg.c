dfb28f2ca6a80341c7cc45bcdf1c32609654450b
tracer-ics2022 <tracer@njuics.org>
2023-01-03 16:47:49 +0800

    >  compile NEMU 2004240422 柯__ Linux zj 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:47:49 up 14 min,  0 users,  load average: 0.02, 0.02, 0.00

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -27,6 +27,7 @@ void isa_reg_display() {
   for (int i = 1; i < 32; i++) {
     printf("%-15s0x%-18lx%ld\n", regs[i], cpu.gpr[i], cpu.gpr[i]);
   }
+  printf("%-15s0x%-18lx%ld\n", "pc", cpu.pc, cpu.pc);
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
